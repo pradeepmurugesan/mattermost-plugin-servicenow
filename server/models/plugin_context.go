962fe9bb787fca7e99b0f32ee1771f091b97a0be
Pradeep Murugesan <pradeep.murugesan@transferwise.com>
2019-10-29 22:37:47 +0530

    fixed lint errors gofmt

--- server/models/plugin_context.go
+++ server/models/plugin_context.go
@@ -6,8 +6,8 @@ import (
 
 // PluginContext context of the plugin
 type PluginContext struct {
-	API         plugin.API
-	BotUserId	string
+	API       plugin.API
+	BotUserId string
 }
 
 func (p *PluginContext) SetApi(api plugin.API) {
